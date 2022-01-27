761e2bc5857bc489c3f97b92511fd78f194d9df6
Namitha-Susan-Mathew <84177289+Namitha-Susan-Mathew@users.noreply.github.com>
2022-01-27 21:33:00 +0530

    Create Add.java
    Program to add two numbers

---
+++ Cycle_1/Add.java
@@ -0,0 +1,13 @@
+/* Program to add two numbers*/
+import java.util.*;
+class Add {
+     public static void main(String args[]){
+         int a,b,sum;
+         System.out.println("Enter two numbers : ");
+         Scanner obj=new Scanner(System.in);
+         a=obj.nextInt();
+         b=obj.nextInt();
+         sum=a+b;
+         System.out.println("Sum of the two numbers is "+sum);
+        }
+    }
